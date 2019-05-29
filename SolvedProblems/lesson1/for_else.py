030582ba91e6f991c45d35ee8d38cee9a4b9c8cc
SOKHUONG UON <uonsokhuong18@kit.edu.kh>
2019-05-29 10:52:27 +0700

    Create for_else.py

---
+++ SolvedProblems/lesson1/for_else.py
@@ -0,0 +1,16 @@
+nums=[13,16,18,27,28]
+for num in nums:
+    if num%5==0:
+        print(num)
+        break
+else:
+    print('Not found')
+
+#let compare with this one!
+nums=[10,16,18,27,28]
+for num in nums:
+    if num%5==0:
+        print(num)
+        break
+else:
+    print('Not found')
